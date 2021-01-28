09deb1c47d1edb094fe82333ebd391d017b8e27a
Parm Singh <parm@snapsuite.io>
2021-01-28 15:35:17 +0800

    importing files

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import {
+  ValidationObserver,
+  ValidationProvider,
+  extend,
+  localize
+} from 'vee-validate'
 
+Vue.component('ValidationObserver', ValidationObserver)
+Vue.component('ValidationProvider', ValidationProvider)
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 Vue.config.productionTip = false
 
 new Vue({
